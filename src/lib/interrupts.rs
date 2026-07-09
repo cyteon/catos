4d670de6cdd57763cdadb60c9338f8f3410f8999
cyteon <me@cyteon.dev>
2026-07-09 12:08:46 +0200

    handle double faults

--- src/lib/interrupts.rs
+++ src/lib/interrupts.rs
@@ -5,6 +5,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.double_fault.set_handler_fn(double_fault_handler);
         idt
     };
 }
@@ -16,4 +17,17 @@ pub fn init() {
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     crate::println!("\n--- EXCEPTION: BREAKPOINT ---");
     crate::println!("{:#?}\n", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) -> ! {
+    crate::println!(
+        "\n--- EXCEPTION: DOUBLE FAULT (error code: {}) ---",
+        error_code
+    );
+    crate::println!("{:#?}\n", stack_frame);
+
+    crate::hlt()
 }
